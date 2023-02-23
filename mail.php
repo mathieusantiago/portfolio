c7a3cddd0f212a9dde8a848b5a24e650386e4f2d
Mathieu Santiago <62888392+mathieusantiago@users.noreply.github.com>
2023-02-23 10:16:47 +0100

    first commit

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+$secret = "6LdmbLgdAAAAACXlaaWGyRVKdfPAJoanoxhy3Jx1";
+$response = htmlspecialchars($_POST['g-recaptcha-response']);
+$remoteip = $_SERVER['REMOTE_ADDR'];
+$request = "https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$remoteip";
+
+$get = file_get_contents($request);
+$decode = json_decode($get, true);
+
+if ($decode['success']) {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['text'])) {
+
+            $recepient = "mathieusantiago21@gmail.com";
+            $sitename = "santiagodevweb";
+
+            $name = strip_tags(trim($_POST["name"]));
+            $email = strip_tags(trim($_POST["email"]));
+            $text = htmlspecialchars(trim($_POST["text"]));
+            $message = "Name: $name \nEmail: $email \nText: $text";
+
+            $pagetitle = "NEW!!! Message du site \"$sitename\"";
+            mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
+        }
+    } else {
+        http_response_code(405);
+        echo "méthode non autorisée";
+    }
+} 
